62f6fbc23b185f3c9ef6f3f2e81882af406117d0
Dan Barber <github@danbarber.me>
2018-04-10 12:54:07 -0400

    Don't need this require

--- lib/middleman-inline_svg.rb
+++ lib/middleman-inline_svg.rb
@@ -1,5 +1,4 @@
 require "middleman-core"
-require "middleman-inline_svg/inline_svg"
 require "middleman-inline_svg/middleman-inline_svg"
 
 ::Middleman::Extensions.register(:inline_svg, MiddlemanInlineSVG)
